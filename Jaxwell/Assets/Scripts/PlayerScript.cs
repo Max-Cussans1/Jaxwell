c0e7a6d30c70225ef3c79068e67fdff291505334
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2019-10-30 16:51:09 +0000

    Jump Functionality fix
    Fixed jump functionality by creating a parent gameobject to the surface, tagging it with JumpableSurface and using collision.gameObject.transform.parent.Comparetag instead of comparing with the actual object. Again, probably not ideal implementation but prototypable for now.

--- Jaxwell/Assets/Scripts/PlayerScript.cs
+++ Jaxwell/Assets/Scripts/PlayerScript.cs
@@ -195,7 +195,7 @@ public class PlayerScript : MonoBehaviour
     void OnCollisionEnter2D(Collision2D collision)
     {
         //check if we are touching a jumpable surface (add tag in editor to surface objects)
-        if (collision.gameObject.CompareTag("JumpableSurface"))
+        if (collision.gameObject.transform.parent.CompareTag("JumpableSurface"))
         {
             Debug.Log("Touched Jumpable Surface");
             canJump = true;
@@ -205,7 +205,7 @@ public class PlayerScript : MonoBehaviour
     void OnCollisionExit2D(Collision2D collision)
     {
         //check if we have left a jumpable surface (add tag in editor to surface objects)
-        if (collision.gameObject.CompareTag("JumpableSurface"))
+        if (collision.gameObject.transform.parent.CompareTag("JumpableSurface"))
         {
             Debug.Log("Left Jumpable Surface");
             canJump = false;
