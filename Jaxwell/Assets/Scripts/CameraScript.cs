4ca40ef2abeb727f056068410a2ea7c3c9398ee4
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2019-10-30 12:51:52 +0000

    Follow Camera Functionality
    Added simple follow camera script, added player gameobject to the script in editor. Can be modified later if needed for potentially smoother camera (bobbing, drifting etc.)

---
+++ Jaxwell/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    //add the player to the script in editor
+    public Transform player;
+
+    //change as needed
+    public float height = -10.0f;
+
+    void Update()
+    {
+        //changes camera position to be above the player poition (added in editor) in the z axis
+        transform.position = new Vector3(player.position.x, player.position.y, height);
+    }
+}
