b6bc493edb9913137af3cf8af4aa94b635e68fd5
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-01-25 12:36:36 +0000

    Scene Portal
    Added a scene portal script and made a prefab that uses a trigger collider to detect if the player is inside it, and if it is to load the scene specified in the editor on the script as a string. Note that build settings will have to include each scene we want to use for this in File>Build Settings.

---
+++ Jaxwell/Assets/Scripts/ScenePortal.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenePortal : MonoBehaviour
+{
+    public string destinationScene;
+
+    PlayerState player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = FindObjectOfType<PlayerState>();
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        //check if whatever we are hitting isn't null
+        if (other.gameObject != null)
+        {
+            if (other.gameObject == player.gameObject)
+            {
+                if (destinationScene != null)
+                {
+                    Debug.Log("Entered scene transition from " + SceneManager.GetActiveScene().name + " to " + destinationScene);
+                    SceneManager.LoadScene(destinationScene);
+                    player.Save();
+                }
+                else
+                {
+                    Debug.Log("Attempted scene transition through scene portal but destination scene was null");
+                }
+            }
+        }
+    }
+}
