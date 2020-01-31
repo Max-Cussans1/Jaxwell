8f1035fbcb53ec976872a862bc2eb4404b830e35
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-01-31 18:12:46 +0000

    Added Position Debug to debug bar
    Added this in a simple script and attached it to the UI prefab.

---
+++ Jaxwell/Assets/Scripts/UI/Debug/Position.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Position : MonoBehaviour
+{
+    GameObject player;
+    Text positionText;
+    Vector2 playerPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        positionText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerPos = player.transform.position;
+        positionText.text = "Position: " + playerPos.ToString();
+    }
+}
