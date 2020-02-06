b3ba07935d184d37820781e954686258d646a95a
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-02-06 00:06:07 +0000

    Platform Oscillation
    Added platform oscillation functionality in the platform scripts with tweakable values in editor. Still need to find a solution to make sure the player moves with the platform when oscillating horizontally.

--- Jaxwell/Assets/Scripts/Platform.cs
+++ Jaxwell/Assets/Scripts/Platform.cs
@@ -10,6 +10,22 @@ public class Platform : Elements
     PlayerState playerstate;
 
     float rangeToCollide;
+    float startX;
+    float startY;
+
+    [SerializeField] bool oscillateVertically = false;
+    [SerializeField] bool oscillateUpwardsFirst = false;
+    [SerializeField] bool oscillateHorizontally = false;
+    [SerializeField] bool oscillateRightFirst = false;
+    [SerializeField] float verticalOscillationDistance = 5.0f;
+    [SerializeField] float verticalOscillationSpeed = 0.05f;
+    [SerializeField] float verticalOscillationPauseTime = 3.0f;
+    [SerializeField] float horizontalOscillationDistance = 5.0f;
+    [SerializeField] float horizontalOscillationSpeed = 0.05f;
+    [SerializeField] float horizontalOscillationPauseTime = 3.0f;
+
+    float tempXOscillationPauseTime;
+    float tempYOscillationPauseTime;
 
     //change to the element we need in editor
     public elements element = 0;
@@ -17,6 +33,12 @@ public class Platform : Elements
     // Start is called before the first frame update
     void Start()
     {
+        startX = transform.position.x;
+        startY = transform.position.y;
+
+        tempYOscillationPauseTime = verticalOscillationPauseTime;
+        tempXOscillationPauseTime = horizontalOscillationPauseTime;
+
         //get platforms collider
         p_collider = GetComponent<BoxCollider2D>();
 
@@ -55,7 +77,18 @@ public class Platform : Elements
                 DebugHelper.Log(this.gameObject + " had collider disabled because player is a different element and in range");
             }
         }
+    }
 
+    void FixedUpdate()
+    {
+        if(oscillateVertically)
+        {
+            oscillateY(ref oscillateUpwardsFirst, startY, verticalOscillationSpeed, ref tempYOscillationPauseTime);
+        }
+        if(oscillateHorizontally)
+        {
+            oscillateX(ref oscillateRightFirst, startX, horizontalOscillationSpeed, ref tempXOscillationPauseTime);
+        }
     }
 
     //returns true if in range
@@ -69,6 +102,88 @@ public class Platform : Elements
         else
         {
             return false;
+        }
+    }
+
+    void oscillateY(ref bool startDirection, float start, float speed, ref float delay)
+    {
+        //if the start direction is true (up) and we're not at the max position
+        if(startDirection && transform.position.y < start + verticalOscillationDistance)
+        {
+            //check if the position change will be above the max distance
+            if (!(transform.position.y + speed > start + verticalOscillationDistance))
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y + speed);
+            }
+            else
+            {
+                delay -= Time.deltaTime;
+                if(delay < 0)
+                {
+                    startDirection = false;
+                    delay = verticalOscillationPauseTime;
+                }
+            }
+        }
+
+        if(!startDirection && transform.position.y > start - verticalOscillationDistance)
+        {
+            //check if the position change will be below the negative max distance
+            if (!(transform.position.y - speed < start - verticalOscillationDistance))
+            {
+                transform.position = new Vector2(transform.position.x, transform.position.y - speed);
+            }
+            else
+            {
+                delay -= Time.deltaTime;
+                if (delay < 0)
+                {
+                    startDirection = true;
+                    delay = verticalOscillationPauseTime;
+                }
+            }
+        }
+    }
+
+    void oscillateX(ref bool startDirection, float start, float speed, ref float delay)
+    {
+        //if the start direction is true (right) and we're not at the max position
+        if (startDirection && transform.position.x < start + horizontalOscillationDistance)
+        {
+            //check if the position change will be above the max distance
+            if (!(transform.position.x + speed > start + horizontalOscillationDistance))
+            {
+                transform.position = new Vector2(transform.position.x + speed, transform.position.y);
+            }
+            else
+            {
+                delay -= Time.deltaTime;
+                if (delay < 0)
+                {
+                    startDirection = false;
+                    delay = horizontalOscillationPauseTime;
+                    DebugHelper.Log("x position = " + transform.position.x + " distance = " + (start - horizontalOscillationDistance));
+                }
+            }
+        }
+
+        if (!startDirection && transform.position.x > start - horizontalOscillationDistance)
+        {
+            //check if the position change will be below the negative max distance
+            if (!(transform.position.x - speed < start - horizontalOscillationDistance))
+            {
+                transform.position = new Vector2(transform.position.x - speed, transform.position.y);
+            }
+            else
+            {
+                delay -= Time.deltaTime;
+                if (delay < 0)
+                {
+                    startDirection = true;
+                    delay = horizontalOscillationPauseTime;
+                    DebugHelper.Log("x position = " + transform.position.x + " distance = " + (start + horizontalOscillationDistance));
+                }
+            }
         }
     }
 
