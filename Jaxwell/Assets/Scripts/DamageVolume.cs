a60a4f1ea5a8de3c3c2bf54fbd1bbd38d1456069
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-02-12 18:43:54 +0000

    Health/Damage + Dying/Respawning
    Set this up pretty simply where if you die you go to the last checkpoint you were at. Made a prefab for a damage volume which takes the specified damage in the script (can be changed in editor) when collided with. After we take damage we check if we're less than 0 health and if we are we respawn at the last checkpoint. May need to change timings for animations or anything else but the basic functionality is there with logging to console.

--- Jaxwell/Assets/Scripts/DamageVolume.cs
+++ Jaxwell/Assets/Scripts/DamageVolume.cs
@@ -2,20 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoint : MonoBehaviour
+public class DamageVolume : MonoBehaviour
 {
-    PlayerState player;    
+    PlayerState player;
+    Health playerHealth;
 
-    public Vector3 position;
+    [SerializeField] int damage = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerState>();
-        position = transform.position;
+        playerHealth = player.GetComponent<Health>();
     }
 
-    
+
     void OnTriggerEnter2D(Collider2D other)
     {
         //check if whatever we are hitting isn't null
@@ -23,9 +24,8 @@ public class Checkpoint : MonoBehaviour
         {
             if (other.gameObject == player.gameObject)
             {                
-                player.currentCheckpoint = position;
-                DebugHelper.Log("New save at " + position);
-                player.Save();
+                playerHealth.TakeDamage(damage);
+                DebugHelper.Log("Player took " + damage + " damage from " + this.gameObject);
             }
         }
     }
---
+++ Jaxwell/Assets/Scripts/Player/Health.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    PlayerState player;
+
+    public bool dead = false;
+
+    public int health = 100;
+    int initialHealth;
+
+    void Start()
+    {
+        player = FindObjectOfType<PlayerState>();
+        initialHealth = health;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        DebugHelper.Log(this.gameObject + " took damage and is at " + health + " health");
+        if (health <= 0)
+        {
+            dead = true;
+            DebugHelper.Log(this.gameObject + " died at " + transform.position + "!");
+            //DEAD STUFF
+            Respawn(player.currentCheckpoint);
+        }
+    }
+
+    void ResetHealth()
+    {        
+        health = initialHealth;
+        dead = false;
+
+        DebugHelper.Log(this.gameObject + " reset health");
+    }
+
+    void Respawn(Vector3 respawnPosition)
+    {
+        //TODO: ADD ELEMENT WE WANT TO RESPAWN AS
+
+        //reset health and move player to respawn location
+        ResetHealth();
+        player.transform.position = respawnPosition;
+
+        DebugHelper.Log(this.gameObject + " respawned at " + respawnPosition);
+    }
+}
