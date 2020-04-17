07a0020282300338776f90103a6ba13777e261e5
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-04-17 21:37:20 +0100

    Tooltip Trigger & Tooltip
    Added a simple text object in our UI canvas prefab (feel free to tweak text/position) and made a quick script for the ability to trigger it and saved a prefab. Basically all the prefab is is a box collider to trigger the tooltip - the script attached to it allows us to change the text of the tooltip, change if we'd like to trigger it more than once and for how long we'd like it to appear. For now just make sure we drag the tooltip game object from the UI canvas into the "Tooltip" variable in editor, and the player game object to the "Player". Would still like to come back to it to make it handle some error cases.

---
+++ Jaxwell/Assets/Scripts/UI/Game_UI/Tooltip.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Tooltip : MonoBehaviour
+{
+    //TODO: ERROR HANDLING FOR IF THESE ARE EMPTY
+    public Text tooltip;
+    public GameObject player;
+
+    public string TooltipText = "";
+    bool textChanged = false;
+
+    public float timeToShow = 10.0f;
+    float tempTimeToShow;
+
+    public bool showOnlyOnce = false;
+    bool hasBeenShown = false;
+    bool showToolTip = false;
+
+    void Start()
+    {
+        tempTimeToShow = timeToShow;
+    }
+
+    void Update()
+    {
+        if(showToolTip)
+        {            
+            if(!tooltip.enabled)
+            {
+                tooltip.enabled = true;
+            }
+            tempTimeToShow -= Time.deltaTime;
+
+            if(tempTimeToShow <= 0)
+            {
+                hasBeenShown = true;
+                showToolTip = false;
+                tooltip.enabled = false;
+
+                if(!showOnlyOnce)
+                {
+                    tempTimeToShow = timeToShow;
+                }
+            }
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        //check if whatever we are hitting isn't null
+        if (other.gameObject != null)
+        {
+            if (other.gameObject == player.gameObject)
+            {
+                if(!textChanged)
+                {
+                    tooltip.text = TooltipText;
+                }
+
+                if (showOnlyOnce)
+                {
+                    if (!hasBeenShown)
+                    {
+                        showToolTip = true;
+                    }
+                }
+                else
+                {
+                    showToolTip = true;
+                }
+            }
+        }
+    }
+
+
+}
