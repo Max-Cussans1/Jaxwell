b8063720ee5e4f6b4fb11a36850b9f1ffac45e53
Max-Cussans1 <44192221+Max-Cussans1@users.noreply.github.com>
2020-06-23 20:39:31 +0100

    Started work on audiomanager
    Started work on an audiomanager script to handle music better and so we can handle sfx cleanly.

---
+++ Jaxwell/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager privateInstance;
+
+    public static AudioManager instance
+    {
+        get
+        {
+            if(privateInstance == null)
+            {
+                //if there is no instance of AudioManager, create one
+                privateInstance = FindObjectOfType<AudioManager>();
+                if(privateInstance == null)
+                {
+                    privateInstance = new GameObject("Created AudioManager", typeof(AudioManager)).GetComponent<AudioManager>();
+                }
+            }
+
+            return privateInstance;
+        }
+        private set
+        {
+            privateInstance = value;
+        }
+    }
+
+    //need 2 music sources to transition between scenes
+    private AudioSource musicSource;
+    private AudioSource musicSource2;
+    private AudioSource sfxSource;
+
+
+}
